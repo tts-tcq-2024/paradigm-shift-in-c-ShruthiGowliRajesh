1f6c30996865e21593af30551e483537e36cd4f2
ShruthiGowliRajesh <shruthi96.grs@gmail.com>
2024-06-26 15:57:39 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -2,23 +2,55 @@
 #include <assert.h>
 #include <stdbool.h>
 
-static bool batteryIsOk(float temperature, float soc, float chargeRate) {
-  if(temperature < 0 || temperature > 45) {
-    printf("Temperature out of range!\n");
-    return false;
-  }
-   if(soc < 20 || soc > 80)  {
-    printf("soc out of range!\n");
-    return false;
-  }
-   if(chargeRate > 0.8) {
-    printf("charge rate out of range!\n");
-    return false;
-  }
-  return true;
-}
-
-int main() {
+static bool IsTempOk(float temperature)
+    {
+         if(temperature < 0 || temperature > 45) {
+            printf("Temperature is out of range!");
+            return false;
+        }
+        else 
+        {
+             printf("Temperature is within Range");
+            return true;
+        }
+    }
+ 
+    static bool IsSOCOk(float soc)
+    {
+         if(soc < 20 || soc > 80)
+         {
+            printf("State of Charge is out of range!");
+            return false;
+         }
+        else  
+        {
+             printf("State of Charge is within Range");
+             return true;
+        }
+    }
+ 
+    static bool IsChargeRateOk(float chargeRate)
+    {
+        if(chargeRate > 0.8) {
+            printf("Charge Rate is out of range!");
+            return false;
+        }
+        else 
+        {
+            printf("Charge Rate is within Range");
+             return true;
+        }
+    }
+    static bool batteryIsOk(float temperature, float soc, float chargeRate) {
+         bool tempok =IsTempOk(temperature);
+         bool socok =IsSOCOk(soc);
+         bool chargerateok =IsChargeRateOk(chargeRate);        
+        return tempok && socok && chargerateok;
+    }
+ 
+ int main() {
   assert(batteryIsOk(25, 70, 0.7));
   assert(!batteryIsOk(50, 85, 0));
 }
+
+
