d50ef29c494d2981b348559163af6f48fbe2750a
ShruthiGowliRajesh <shruthi96.grs@gmail.com>
2024-06-26 14:58:20 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -4,7 +4,7 @@
 int batteryIsOk(float temperature, float soc, float chargeRate) {
   if((temperature < 0 || temperature > 45) || (soc < 20 || soc > 80) ||chargeRate > 0.8) {
     printf("Temperature/soc/charge rate out of range!\n");
-    return 0;
+    
   }
   return 1;
 }
